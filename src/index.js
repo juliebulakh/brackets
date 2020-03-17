013b03ff32f8112c79edfc08b9d46252f9979509
juliebulakh <bulakh.julie@gmail.com>
2020-03-17 23:30:26 +0300

    fix: fix conditions solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
  
-    let stek = [];
-    let brakets = bracketsConfig.flat(99).join('');
-  
-      for(i=0;i<str.length;i++){
-        if (brakets.indexOf(str[i]) % 2 == 0&&stek[stek.length - 1]!==str[i]) {
-          stek.push(str[i]);
-          }
-          else if (stek.length && (brakets.includes(stek[stek.length - 1] + str[i]))) {
-              stek.pop();
-          }
-          else return false;
-    }
-       return stek.length>0?false:true;
+  let stek = [];
+  let brakets = bracketsConfig.flat(1).join('');
+
+  for (i = 0; i < str.length; i++) {
+    if (brakets.indexOf(str[i]) % 2 ==0&&stek[stek.length - 1]!==brakets[brakets.indexOf(str[i])+1]) {
+        stek.push(str[i]);
+        }
+        else if (stek.length && (brakets.includes(stek[stek.length - 1] + str[i]))) {
+            stek.pop();
+        }
+        else return false;
+  }
+     return stek.length>0?false:true;
   
   }
