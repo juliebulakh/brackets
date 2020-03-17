40ae8ca750e08dd65e77585737f2474e76eca498
juliebulakh <bulakh.julie@gmail.com>
2020-03-17 23:17:12 +0300

    init: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ 
+    let stek = [];
+    let brakets = bracketsConfig.flat(1).join('');
+  
+      for(i=0;i<str.length;i++){
+        if (brakets.indexOf(str[i]) % 2 == 0&&stek[stek.length - 1]!==str[i]) {
+          stek.push(str[i]);
+          }
+          else if (stek.length && (brakets.includes(stek[stek.length - 1] + str[i]))) {
+              stek.pop();
+          }
+          else return false;
+    }
+       return stek.length>0?false:true;
+  
+  }
