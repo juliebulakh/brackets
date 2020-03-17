b38a896325f4593f1c84f643a6bb3a06e1dc34b3
juliebulakh <bulakh.julie@gmail.com>
2020-03-17 23:19:30 +0300

    feat: fix solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
  
     let stek = [];
-    let brakets = bracketsConfig.flat(1).join('');
+    let brakets = bracketsConfig.flat(99).join('');
   
       for(i=0;i<str.length;i++){
         if (brakets.indexOf(str[i]) % 2 == 0&&stek[stek.length - 1]!==str[i]) {
